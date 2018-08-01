a96dc1561ca180bc296fc531a1358848b4c75e8d
douglasjd1 <douglasjd09@gmail.com>
2018-08-01 12:20:11 -0500

    Tried google maps

---
+++ public/javascripts/map.js
@@ -0,0 +1,10 @@
+function initMap(){
+      var map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 8
+        });
+      }
+}
+
